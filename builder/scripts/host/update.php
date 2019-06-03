95e1e127ec3acccbedbb46d1247cad6fdebdc36e
Nadav Tasher <tashernadav@gmail.com>
2019-06-03 11:21:50 +0300

    Things

--- builder/scripts/host/update.php
+++ builder/scripts/host/update.php
@@ -14,14 +14,14 @@ echo "Done\n";
 $master = new stdClass();
 foreach ($assortment as $flavour) {
     echo "\n";
-    removeDirectory(TEMPORARY_DIRECTORY);
-    removeDirectory(TEMPORARY_FILE);
+    builder_rmdir(TEMPORARY_DIRECTORY);
+    builder_rmdir(TEMPORARY_FILE);
     $repository_zip = $flavour . "/archive/master.zip";
     echo "Downloading template from \"" . $repository_zip . "\" - ";
     file_put_contents(TEMPORARY_FILE, file_get_contents($repository_zip));
     echo "Done\n";
     echo "Extracting to \"" . TEMPORARY_DIRECTORY . "\" - ";
-    unzip(TEMPORARY_FILE, TEMPORARY_DIRECTORY);
+    builder_unzip(TEMPORARY_FILE, TEMPORARY_DIRECTORY);
     echo "Done\n";
     $repository_directory = glob(TEMPORARY_DIRECTORY . DIRECTORY_SEPARATOR . "*", GLOB_ONLYDIR)[0];
     if (file_exists($repository_directory . DIRECTORY_SEPARATOR . TEMPLATE_FILE)) {
@@ -31,13 +31,13 @@ foreach ($assortment as $flavour) {
         $master->$template_name = $template->replacements;
         echo "Done\n";
         echo "Compressing to \"" . FLAVOUR_DIRECTORY . DIRECTORY_SEPARATOR . $template_name . ".zip" . "\" - ";
-        rezip(FLAVOUR_DIRECTORY . DIRECTORY_SEPARATOR . $template_name . ".zip", $repository_directory);
+        builder_zip(FLAVOUR_DIRECTORY . DIRECTORY_SEPARATOR . $template_name . ".zip", $repository_directory);
         echo "Done\n";
     }
 }
 echo "\n";
-removeDirectory(TEMPORARY_DIRECTORY);
-removeDirectory(TEMPORARY_FILE);
+builder_rmdir(TEMPORARY_DIRECTORY);
+builder_rmdir(TEMPORARY_FILE);
 echo "Writing master list - ";
 file_put_contents(MASTER_LIST, json_encode($master));
 echo "Done\n";
